777316e0f8f0465c6cfbd7f9be814047a5f07d5e
Carlo Zapponi <littleark@gmail.com>
2012-01-20 19:59:28 +0100

    fixed issue when adding to the DOM

--- sopawall.js
+++ sopawall.js
@@ -1,21 +1,20 @@
+//     sopawall.js
+//     (c) 2012 Thomas Fuchs
+//     Use it, change it, modify it...do whatever you want with this piece of code
 (function(){
 		try {
-			window.createTweets=function(tweets){
+			window.sopaWallCreateTweets=function(tweets){
 				sopa.setTweets(tweets);
 			}
-		
-			var sopastyle="#sopawall{z-index:9999;position:absolute;top:0;left:0;right:0;min-height:50px;}#sopawall a{line-height:46px;background-color:#000;color:#000;height:26px;font-size:18px;font-family:Georgia,serif;text-decoration:none;-webkit-transition:background .5s ease-out;-moz-transition:background .5s ease-out;-o-transition:background .5s ease-out;transition:background .5s ease-out;margin:0;padding:10px;}#sopawall a:hover{color:#fff;text-decoration:underline;}#sopawall a.last-child,#sopawall a.last-child:hover{background-color:#fff;color:#000;}#sopawall a.top{position:absolute;top:0;display:block;text-decoration:none;background-color:#fff;z-index:9999;font-size:12px;line-height:14px;height:auto;padding:4px 8px;}#sopawall a.top:hover{text-decoration:underline;color:#00aeff;}#sopawall #closesopa{right:5px;}#sopawall #sopatwitter{right:95px;}#sopawall #sopalink{right:160px;}";
-			var sopadom="<style type=\"text/css\" media=\"screen\">"+sopastyle+"</style><div id=\"sopawall\"></div>";
 		
 			function Sopa(){
 			
 				var data={
-					last:new Date().getTime(),
 					tweets:{},
 					t:1
-				};
-			
-				var sopa={};
+				},
+				sopa={},
+				sopastyle="#sopawall{z-index:9999;position:absolute;top:0;left:0;right:0;min-height:50px;}#sopawall a{line-height:46px;background-color:#000;color:#000;height:26px;font-size:18px;font-family:Georgia,serif;text-decoration:none;-webkit-transition:background .5s ease-out;-moz-transition:background .5s ease-out;-o-transition:background .5s ease-out;transition:background .5s ease-out;margin:0;padding:10px;}#sopawall a:hover{color:#fff;text-decoration:underline;}#sopawall a.last-child,#sopawall a.last-child:hover{background-color:#fff;color:#000;}#sopawall a.top{position:absolute;top:0;display:block;text-decoration:none;background-color:#fff;z-index:9999;font-size:12px;line-height:14px;height:auto;padding:4px 8px;}#sopawall a.top:hover{text-decoration:underline;color:#00aeff;}#sopawall #closesopa{right:5px;}#sopawall #sopatwitter{right:95px;}#sopawall #sopalink{right:160px;}";
 
 				this.setTweets=function(tweets) {
 					data.tweets=tweets;
@@ -46,11 +45,24 @@
 				}
 			
 				;(function init(){
-					var body=document.getElementsByTagName("body")[0];
-					body.innerHTML=body.innerHTML+sopadom;
-					sopa=document.getElementById("sopawall");
-				
-					var sopalink=document.createElement("a");
+					
+					var body=document.getElementsByTagName("body")[0],
+						cssNode = document.createElement('style'),
+						sopalink=document.createElement("a"),
+						twitter=document.createElement("a"),
+						close=document.createElement("a");
+						
+					cssNode.type = 'text/css';
+					cssNode.rel = 'stylesheet';
+					cssNode.media = 'screen';
+					cssNode.innerHTML=sopastyle;
+					body.appendChild(cssNode);
+					
+					
+					sopa=document.createElement("div");
+					sopa.id="sopawall";
+					body.appendChild(sopa);
+					
 					sopalink.id="sopalink";
 					sopalink.className="top";
 					sopalink.href="https://github.com/littleark/SOPA-WALL";
@@ -58,7 +70,6 @@
 					sopalink.target="blank_";
 					sopa.appendChild(sopalink);
 
-					var twitter=document.createElement("a");
 					twitter.id="sopatwitter";
 					twitter.className="top";
 					twitter.href="https://twitter.com/intent/tweet?hashtags=SOPA&original_referer="+(encodeURI(location.href))+"&source=tweetbutton&text="+(encodeURI(location.href+" has been auto-censored with SOPA WALL. Say no to SOPA and PIPA"))+"&url="+(encodeURI("https://github.com/littleark/SOPA-WALL"))+"&via=littleark";
@@ -66,7 +77,6 @@
 					twitter.target="blank_";
 					sopa.appendChild(twitter);
 				
-					var close=document.createElement("a");
 					close.id="closesopa";
 					close.className="top";
 					close.href="#";
@@ -77,13 +87,11 @@
 						close.addEventListener("click", function(e){e.preventDefault();clearTimeout(data.t);data.t=false;body.removeChild(sopa);sopa={};return false;}, false);
 					}
 					sopa.appendChild(close);
-				
+					
 					var jsonp=document.createElement("script");
 					jsonp.setAttribute("type", "text/javascript")
 					jsonp.src="http://www.makinguse.com/sopawall/getTweets.php";
 					body.appendChild(jsonp);
-				
-					//loop();
 				
 				}());
 			}
